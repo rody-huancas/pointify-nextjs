fc1ebe42ed571267a1e13ee1b61a559ed2f8ee5b
Rody Huancas <rodyhuancas.04@gmail.com>
2024-05-16 20:12:00 -0500

    Add: configuration initial

---
+++ tailwind.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "hsl(var(--primary))"
+        },
+        background: {
+          DEFAULT: "hsl(var(--background))",
+          foreground: "hsl(var(--background-foreground))"
+        },
+        text: {
+          DEFAULT: "hsl(var(--text))",
+        },
+      },
+      borderRadius: {
+        "4xl": "2rem"
+      },
+      transitionDuration: {
+        DEFAULT: "150ms"
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
